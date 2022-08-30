1a9a374aa7a2724513c6ac8312021fc7ecb7fb12
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-30 08:02:19 +0700

    #5 - Exclude ThymeleafAutoConfiguration.class :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/SpringBootMailgunTemplateApplication.java
+++ src/main/java/com/hendisantika/SpringBootMailgunTemplateApplication.java
@@ -2,8 +2,9 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ThymeleafAutoConfiguration.class})
 public class SpringBootMailgunTemplateApplication {
 
     public static void main(String[] args) {
