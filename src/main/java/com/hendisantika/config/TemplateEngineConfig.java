4290ef1c38a81121f957f13ce17315431a2d0444
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-30 08:03:38 +0700

    #8 - Add emailTemplateResolver in TemplateEngineConfig :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/config/TemplateEngineConfig.java
+++ src/main/java/com/hendisantika/config/TemplateEngineConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.Collections;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +34,19 @@ public class TemplateEngineConfig implements WebMvcConfigurer {
         templateEngine.addTemplateResolver(emailTemplateResolver());
 
         return templateEngine;
+    }
+
+
+    private ITemplateResolver emailTemplateResolver() {
+        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setOrder(1);
+        templateResolver.setResolvablePatterns(Collections.singleton("*"));
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding(EMAIL_TEMPLATE_ENCODING);
+        templateResolver.setCacheable(false);
+
+        return templateResolver;
     }
 }
